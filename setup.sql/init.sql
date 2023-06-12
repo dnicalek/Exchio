fb996078820b74caf9be5618f523c02347c56710
Nogowicz <nogovitz00@gmail.com>
2023-06-12 14:25:53 +0200

    Add task screen implemented

--- setup.sql/init.sql
+++ setup.sql/init.sql
@@ -1,10 +1,22 @@
-CREATE DATABASE IF NOT EXISTS exchio;
+CREATE DATABASE IF NOT EXISTS todoapp;
 
-USE exchio;
+USE todoapp;
 
 CREATE TABLE IF NOT EXISTS users (
   username VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS tasks (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  taskName VARCHAR(255) NOT NULL,
+  deadline DATE NOT NULL,
+  notes TEXT,
+  priority ENUM('high', 'medium', 'low') NOT NULL,
+  userName VARCHAR(255) NOT NULL,
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  status ENUM('todo', 'inprogress', 'completed') DEFAULT 'todo' NOT NULL
 );
